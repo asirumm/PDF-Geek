50d1e6724e1cdd9b5e22d5e4407a9fee0c2c05ee
haslam <haslamhdz71@gmail.com>
2025-07-06 10:57:34 +0700

    update main

--- src/main/java/org/diagastika/pdfgeek/HelloApplication.java
+++ src/main/java/org/diagastika/pdfgeek/HelloApplication.java
@@ -1,20 +1,31 @@
 package org.diagastika.pdfgeek;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.diagastika.pdfgeek.Helper.ApplicationConfig;
 
 import java.io.IOException;
-
 public class HelloApplication extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        ApplicationConfig config = new ApplicationConfig();
+        config.createFolderForApp();
+
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource(Constant.HOME_FXML));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 500);
+        stage.setTitle("PDF-Geek");
+
+        stage.setResizable(false);
+
         stage.setScene(scene);
         stage.show();
+
+        // for gc
+        config = null;
     }
 
     public static void main(String[] args) {
