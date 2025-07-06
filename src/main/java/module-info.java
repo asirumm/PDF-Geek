0b1380f57a71db09f64b89350cb87bbde0351e61
haslam <haslamhdz71@gmail.com>
2025-07-06 17:08:33 +0700

    production setup

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,14 @@
 module org.diagastika.pdfgeek {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires javafx.swing;
+    requires org.apache.pdfbox;
+    requires org.slf4j;
+    requires jdk.compiler;
+    requires jdk.jconsole;
 
     opens org.diagastika.pdfgeek to javafx.fxml;
     exports org.diagastika.pdfgeek;
+    exports org.diagastika.pdfgeek.Controller;
+    opens org.diagastika.pdfgeek.Controller to javafx.fxml;
 }
